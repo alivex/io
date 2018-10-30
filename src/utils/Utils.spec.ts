7ca78e25987df08a24b4c230b282fd0b7394e103
Micaël Mbagira <micael.mbagira@icloud.com>
2018-10-30 16:34:34 +0100

    Add BinaryStream and JsonStream implementations and remove @advertima/js-libs dependency

---
+++ src/utils/Utils.spec.ts
@@ -0,0 +1,16 @@
+import test from 'ava';
+import { stub } from 'sinon';
+import * as browserEnv from 'browser-env';
+import { Utils } from './Utils';
+
+browserEnv(['window']);
+
+test('should return the value of the search param "test"', t => {
+  stub(window, 'location').value({ search: '?test=foo' });
+  t.is(Utils.getParam('test', 'bar'), 'foo');
+});
+
+test('should return the default value when the requested search param is missing', t => {
+  stub(window, 'location').value({ search: '?test=foo' });
+  t.is(Utils.getParam('hello', 'bar'), 'bar');
+});
